f913df20778e6cfcadc0c9afec16c231deb9e253
Zhe <zf2082@nyu.edu>
2023-02-22 12:43:43 -0500

    Homework 1 upload

---
+++ MoveZeros.java
@@ -0,0 +1,13 @@
+// homework1
+public class MoveZeros {
+    public void moveZeroes(int[] nums) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        for (;k < nums.length; k++) nums[k] = 0;
+    }
+}
