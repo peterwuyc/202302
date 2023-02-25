0cfd6327107d7615030f943f96155c578223330b
Zhe <zf2082@nyu.edu>
2023-02-25 18:46:43 -0500

    homework 1 update

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -1,22 +1,13 @@
 // homework1
+// leetcode 88
 public class MergeSortedArray {
     public void merge(int[] nums1, int m, int[] nums2, int n){
         int k = n + m - 1;
         int i = m - 1, j = n - 1;
         while (i >= 0 && j >= 0){
-            if (nums1[i] >= nums2[j]){
-                nums1[k] = nums1[i];
-                i--;
-            }else{
-                nums1[k] = nums2[j];
-                j--;
-            }
-            k--;
+            if (nums1[i] >= nums2[j]) nums1[k--] = nums1[i--];
+            else nums1[k--] = nums2[j--];
         }
-        while (j >= 0) {
-            nums1[k] = nums2[j];
-            j--;
-            k--;
-        }
+        while (j >= 0) nums1[k--] = nums2[j--];
     }
 }
